84aa443859288ef332fb4bd4ed6a88dc54b10a93
irakeshbind <rakeshbind351998@gmail.com>
2026-01-08 13:51:02 +0530

    name vale is not define

--- frontend/vite-project/src/components/Signup.jsx
+++ frontend/vite-project/src/components/Signup.jsx
@@ -57,7 +57,7 @@ const Signup = () => {
                 <input
                   type="text"
                   placeholder="Input full name"
-                  value={name}
+                  // value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
@@ -69,7 +69,7 @@ const Signup = () => {
                 <input
                   type="text"
                   placeholder="Input full name"
-                  value={name}
+                  // value={email}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
@@ -81,7 +81,7 @@ const Signup = () => {
                 <input
                   type="text"
                   placeholder="Input full name"
-                  value={name}
+                  // value={password}
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
