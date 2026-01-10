015a9cdc69b494e3372e20b7a5c7ba1e96b50996
irakeshbind <rakeshbind351998@gmail.com>
2026-01-10 14:00:41 +0530

    moptro element card add

--- frontend/vite-project/src/components/Dashboard.jsx
+++ frontend/vite-project/src/components/Dashboard.jsx
@@ -4,10 +4,14 @@ import Cart from "../assets/cart.png";
 import signup from "../assets/ds.png";
 import dp from "../assets/dp.png";
 import Sig from "../assets/sig.png";
-import Frame from '../assets/frame.png'
-import Team from '../assets/team.png'
-import Pro from '../assets/pro.png'
-import Teap from '../assets/teap.png'
+import Frame from "../assets/frame.png";
+import Team from "../assets/team.png";
+import Pro from "../assets/pro.png";
+import Teap from "../assets/teap.png";
+import Car from "../assets/car.png";
+import Opp from "../assets/opp.png";
+
+
 
 const Dashboard = () => {
   return (
@@ -57,7 +61,9 @@ const Dashboard = () => {
         <div className="proflie-left-text">
           <div>
             <div className="profile-text-p">
-              <p>Pages /<span className="span-text">Dashboard</span> </p>
+              <p>
+                Pages /<span className="span-text">Dashboard</span>{" "}
+              </p>
             </div>
 
             <div className="profile-text-span">
@@ -83,39 +89,145 @@ const Dashboard = () => {
 
         {/* header 1 */}
         <div className="header-text">
-        <div className="frame-text">
-         <p> <img className="frame-img" src={Frame} /></p>
-        <div className="frame-text-mark">
-             <h5>Mark Johnson</h5>
-         <p>mark@simmmple.com</p>
-        </div>
-        </div>
-        <div className="">
-            <div className="Team-text">
-               <p className="over-text"> <img className="over-img" src={Team}/>OVERVIEW</p>
-                <p><img className="over-img" src={Teap}/>TEAMS</p>
-                <p><img className="over-img" src={Pro}/>PROJECTS</p>
+          <div className="frame-text">
+            <p>
+              {" "}
+              <img className="frame-img" src={Frame} />
+            </p>
+            <div className="frame-text-mark">
+              <h5>Mark Johnson</h5>
+              <p>mark@simmmple.com</p>
             </div>
-
-        </div>
-
-      </div>
-
-        {/* alug second */}
-      <div className="main-header">
-        <div className="main-header-right">
-          <div className="main-text">
-            <h5>MOPTro Informations</h5>
-            <p>Hello, Mark Johnson! Your MOPTro is ready.</p>
+          </div>
+          <div className="">
+            <div className="Team-text">
+              <p className="over-text">
+                {" "}
+                <img className="over-img" src={Team} />
+                OVERVIEW
+              </p>
+              <p>
+                <img className="over-img" src={Teap} />
+                TEAMS
+              </p>
+              <p>
+                <img className="over-img" src={Pro} />
+                PROJECTS
+              </p>
+            </div>
           </div>
         </div>
-        <div className="main-header-left">ff</div>
-      </div>
-      </div>
-    
 
-      
-      
+        {/* alug second */}
+        <div className="main-header">
+          <div className="main-header-right">
+            <div className="main-text">
+              <h5>MOPTro Informations</h5>
+              <p>Hello, Mark Johnson! Your MOPTro is ready.</p>
+            </div>
+
+            <div>
+              <div className="circle-main">
+                <div className="main-flex">
+                  <div>
+                    <div className="circle-text">
+                      <h5>68%</h5>
+                      <p>charging</p>
+                    </div>
+                    <div className="circle-text-h">
+                      <h5>0h 58 min</h5>
+                      <p>Time to full charge</p>
+                    </div>
+                  </div>
+
+
+                  <div className="main-left">
+                   <div className="main-text-left">
+                          <div className="bb">
+
+                            <div className="main-p-text">
+                             <div className="main-per"> 
+                              <div className="main-text-h">
+                                <h6>Battery Health</h6>
+                              <p>76%</p>
+                              </div>
+                              <div><img className="main-image"src={Car}/></div>
+                              
+                              </div>
+                            
+                            </div>
+                            
+                            
+                            {/* s */}
+                            <div className="main-p-text">
+                             <div className="main-per"> 
+                              <div className="main-text-h">
+                                <h6>Efficiency</h6>
+                              <p>+20%</p>
+                              </div>
+                              <div><img className="main-image-ef"src={Car}/></div>
+                              
+                              </div>
+                            
+                            </div>
+
+                            {/* n */}
+                            <div className="main-p-text">
+                             <div className="main-per"> 
+                              <div className="main-text-h">
+                                <h6>Consumption</h6>
+                              <p>163W/km</p>
+                              </div>
+                              <div><img className="main-image"src={Opp}/></div>
+                              
+                              </div>
+                            
+                            </div>
+
+                             <div className="main-p-text">
+                             <div className="main-per"> 
+                              <div className="main-text-h">
+                                <h6>This Week</h6>
+                              <p>1.342/km</p>
+                              </div>
+                              <div><img className="main-image-th"src={Car}/></div>
+                              
+                              </div>
+                            
+                            </div>
+
+
+                            
+
+                             
+                             
+
+
+
+
+                           
+                         
+
+                         
+                          </div>
+
+
+
+                           
+                   </div>
+                   
+                  </div>
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="main-header-left">ff</div>
+        </div>
+        {/* third setting */}
+        <div className="main-setting">ddd</div>
+      </div>
     </div>
   );
 };
